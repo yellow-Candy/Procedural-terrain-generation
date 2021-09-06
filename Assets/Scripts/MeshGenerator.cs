67d4b4e0b4ad5b9753ebeca049496f25851b428b
bhargav <bhargav.bk27@gmail.com>
2021-09-06 23:05:48 +0530

    update on mesh generator (writen basic setup - left to refector)

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -2,8 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[ExecuteInEditMode]
 public class MeshGenerator : MonoBehaviour
 {
+    const int threadGroupSize = 8;
+
+    public bool autoUpdateInEditor = true;
+    public bool autoUpdateInGame = true;
+    public ComputeShader shader;
+
+    [Range(2, 1000)]
+    public int numPointsPerAxis = 30;
+    public float isoLevel;
+
+    bool settingsUpdated;
+
+    ComputeBuffer triangleBuffer;
+    ComputeBuffer triCountBuffer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +30,100 @@ public class MeshGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (settingsUpdated) {
+            RequestMeshUpdate();
+            settingsUpdated = false;
+        }
         // chunk 
         // position
         // number of voxels
     }
+
+    public void Run() {
+        CreateBuffers();
+
+        UpdateChunkMesh();
+    }
+
+    public void RequestMeshUpdate() {
+        if (!Application.isPlaying && autoUpdateInEditor) {
+            Run();
+        }
+    }
+
+    public void UpdateChunkMesh(Chunk chunk) {
+        int numVoxelsPerAxis = numPointsPerAxis - 1;
+        int numThreadsPerAxis = Mathf.CeilToInt(numVoxelsPerAxis / (float)threadGroupSize);
+
+        int numVoxels = numVoxelsPerAxis * numVoxelsPerAxis * numVoxelsPerAxis;
+        int maxTriangleCount = numVoxels * 5;
+
+
+
+        triangleBuffer = new ComputeBuffer(maxTriangleCount, sizeof(float) * 3 * 3, ComputeBufferType.Append);
+        triCountBuffer = new ComputeBuffer(1, sizeof(int), ComputeBufferType.Raw);
+
+        triangleBuffer.SetCounterValue(0);
+        shader.SetBuffer(0, "triangles", triangleBuffer);
+        shader.SetInt("numPointsPerAxis", numPointsPerAxis);
+        shader.SetFloat("isoLevel", isoLevel);
+
+        shader.Dispatch(0, numThreadsPerAxis, numThreadsPerAxis, numThreadsPerAxis);
+
+
+
+        // Get number of triangles in the triangle buffer
+        ComputeBuffer.CopyCount(triangleBuffer, triCountBuffer, 0);
+        int[] triCountArray = { 0 };
+        triCountBuffer.GetData(triCountArray);
+        int numTris = triCountArray[0];
+
+
+        
+        // Get triangle data from shader
+        Triangle[] tris = new Triangle[numTris];
+        triangleBuffer.GetData(tris, 0, 0, numTris);
+
+        
+        
+        Mesh mesh = chunk.mesh;
+        mesh.Clear();
+
+        var vertices = new Vector3[numTris * 3];
+        var meshTriangles = new int[numTris * 3];
+
+        for (int i = 0; i < numTris; i++) {
+            for (int j = 0; j < 3; j++) {
+                meshTriangles[i * 3 + j] = i * 3 + j;
+                vertices[i * 3 + j] = tris[i][j];
+            }
+        }
+        mesh.vertices = vertices;
+        mesh.triangles = meshTriangles;
+
+        mesh.RecalculateNormals();
+    }
+
+
+    struct Triangle {
+#pragma warning disable 649 // disable unassigned variable warning
+        public Vector3 a;
+        public Vector3 b;
+        public Vector3 c;
+
+        public Vector3 this[int i] {
+            get {
+                switch (i) {
+                    case 0:
+                        return a;
+                    case 1:
+                        return b;
+                    default:
+                        return c;
+                }
+            }
+        }
+    }
+
 }
